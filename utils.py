2f61b0103c5e6c6f760eaef034ed1158b0b08e31
em4n0n <58372945+em4n0n@users.noreply.github.com>
2023-10-11 23:32:13 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
 import lzma
 import dill as pickle
 
+def load_pickle(path):
+    with lzma.open(path,"rb") as fp:
+        file = pickle.load(fp)
+    return file
+
+def save_pickle(path):
+    pass
+
+def save_pickle(path,obj):
+    with open(path,"wb") as fp:
+        pickle.dump(obj,fp)
